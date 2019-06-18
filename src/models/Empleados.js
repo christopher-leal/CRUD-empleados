7392d149ac0a8720d192a21d759c614ecdd45af4
Christopher Leal <chris_t_o@hotmail.com>
2019-06-18 12:00:58 -0500

    Creamos el modelo con sequelize para interactuar con la DB

--- src/models/Empleados.js
+++ src/models/Empleados.js
@@ -1,3 +1,20 @@
 const Sequelize = require("sequelize");
+const db = require("../config/database");
 
-const db = require("../config");
+//definicion del modelo empleado para tener comunicacion con la bd
+const Empleado = db.define("empleado", {
+  name: {
+    type: Sequelize.STRING
+  },
+  last_name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.STRING
+  }
+});
+
+module.exports = Empleado;
